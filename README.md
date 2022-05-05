7022b1a74c62e9f1092c08cbd827d0a3bf6fab53
Jwade1781 <jwade5219@gmail.com>
2022-05-04 20:15:23 -0400

    Added music attribution per their usage

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # etch-a-sketch
 Simple Etch a sketch program to practice JS.
+
+## Music
+The Beginning (w Caturday) by Babasmas | https://soundcloud.com/babasmasmoosic
+                Music promoted by https://www.chosic.com/free-music/all/
+                Creative Commons CC BY-SA 3.0
+                https://creativecommons.org/licenses/by-sa/3.0/
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 let randomHoverColor = false;
+let mute = true;
+
 const RANDOM_COLOR_TEXT_MAP = {
     false: "Black & White",
     true: "Random Color"
 };
+
+const MUTE_TEXT_MAP = {
+    false: "Sound On",
+    true: "Sound Off"
+};
+
 
 // Construct initial grid
 window.onload = () => {
@@ -14,7 +22,7 @@ window.onload = () => {
 
 function constructGrid(gridNum, randomColor) {
     // Reconstruct the grid, one was already created
-    if (document.querySelector(".square") !== null){
+    if (document.querySelector(".square") !== null) {
         let squares = document.querySelectorAll(".square");
         squares.forEach((square) => {
             square.parentNode.removeChild(square);
@@ -32,7 +40,7 @@ function constructGrid(gridNum, randomColor) {
         let grid = document.querySelector(".grid");
         let square = document.createElement("div");
         square.setAttribute("class", "square");
-        randomColor? color = getRandomColor() : color = "white";
+        randomColor ? color = getRandomColor() : color = "white";
         square.style.backgroundColor = color;
         square.style.height = squareHeight;
         square.style.width = squareWidth;
@@ -45,98 +53,147 @@ function constructGrid(gridNum, randomColor) {
     }
 }
 
-function addEvents(){
-    // RESET Button
-    let btn = document.querySelector(".resetBtn");
-    btn.addEventListener("click", () => {
+function addEvents() {
+    const resetBtnEvent = () => {
+        let resetBtn = document.querySelector(".resetBtn");
+        resetBtn.addEventListener("click", () => {
 
-        let squares = document.querySelectorAll(".square");
-        squares.forEach((square) => {
-            /*
-            const ANIMATION_NAME = "fadeOutAnimation";
+            let squares = document.querySelectorAll(".square");
+            squares.forEach((square) => {
+                randomHoverColor ? square.style.backgroundColor = getRandomColor() : square.style.backgroundColor = "white";
+            });
 
-            square.classList.add(ANIMATION_NAME);
-            square.addEventListener("webkitAnimationEnd", () => {
+            const ETCH_SKETCH = document.querySelector(".etchSketch");
+            const ANIMATION_NAME = "shakeAnimation";
+
+            ETCH_SKETCH.classList.add(ANIMATION_NAME);
+            ETCH_SKETCH.addEventListener("webkitAnimationEnd", () => {
                 ETCH_SKETCH.classList.remove(ANIMATION_NAME);
             });
-            */
-            randomHoverColor ? square.style.backgroundColor = getRandomColor() : square.style.backgroundColor = "white";
         });
-
-        const ETCH_SKETCH  = document.querySelector(".etchSketch");
-        const ANIMATION_NAME = "shakeAnimation";
-
-        ETCH_SKETCH.classList.add(ANIMATION_NAME);
-        ETCH_SKETCH.addEventListener("webkitAnimationEnd", () => {
-            console.log("Transition End");
-            ETCH_SKETCH.classList.remove(ANIMATION_NAME);
-        });
-    });
-
-    // RANDOM COLOR Button
-    btn = document.querySelector(".randomColorBtn");
-    btn.addEventListener("click", () => {
-        (randomHoverColor) ? randomHoverColor = false : randomHoverColor = true;
-        btn.textContent = RANDOM_COLOR_TEXT_MAP[randomHoverColor];
-
-    });
-
-    // GRID Slider
-    let slider = document.querySelector(".rangeSlider");
-    let sliderOutput = document.querySelector(".sliderOutput");
-    slider.oninput = function() {
-        sliderOutput.textContent = this.value;
     }
 
-    slider.addEventListener("mouseup", () => {
-        constructGrid(slider.value, randomHoverColor);
-    });
+    const randomColorBtnEvent = () => {
+        let colorBtn = document.querySelector(".randomColorBtn");
+        colorBtn.addEventListener("click", () => {
+            (randomHoverColor) ? randomHoverColor = false : randomHoverColor = true;
+            colorBtn.textContent = RANDOM_COLOR_TEXT_MAP[randomHoverColor];
+        });
+    };
+
+    const muteBtnEvent = () => {
+        muteBtn = document.querySelector(".muteBtn");
+        muteBtn.addEventListener("click", () => {
+            music = document.querySelector(".music");
+            if (mute){   
+                mute = false;
+                music.play();
+            }
+            else {
+                mute = true;
+                music.pause();
+            }
+
+            muteBtn.textContent = MUTE_TEXT_MAP[mute];
+        })
+    };
+
+    const gridSliderEvent = () => {
+        let slider = document.querySelector(".rangeSlider");
+        let sliderOutput = document.querySelector(".sliderOutput");
+        slider.oninput = function () {
+            sliderOutput.textContent = this.value;
+        }
+
+        slider.addEventListener("mouseup", () => {
+            constructGrid(slider.value, randomHoverColor);
+        });
+    }
+
+    resetBtnEvent();
+    randomColorBtnEvent();
+    gridSliderEvent();
+    muteBtnEvent();
 }
 
-function addDynamicStartingUI(){
-    const SLIDER = document.querySelector(".rangeSlider");
-    const SLIDER_OUTPUT = document.querySelector(".sliderOutput");
+function addDynamicStartingUI() {
+    const createSlider = () => {
+        // Grid Slider Selection
+        const SLIDER = document.querySelector(".rangeSlider");
+        const SLIDER_OUTPUT = document.querySelector(".sliderOutput");
 
-    SLIDER.value = Math.sqrt(document.querySelectorAll(".square").length);
-    SLIDER_OUTPUT.textContent = SLIDER.value;
+        SLIDER.value = Math.sqrt(document.querySelectorAll(".square").length);
+        SLIDER_OUTPUT.textContent = SLIDER.value;
+    };
+
+    const createColorBtnText = () => {
+        // Random Color Button text
+        const BTN = document.querySelector(".randomColorBtn");
+        BTN.textContent = RANDOM_COLOR_TEXT_MAP[randomHoverColor];
+    };
 
 
-    // Random Color Button text
-    const BTN = document.querySelector(".randomColorBtn");
-    BTN.textContent = RANDOM_COLOR_TEXT_MAP[randomHoverColor];
 
-    const CIRCLE_TOTAL = 50;
-    for (let i = 0; i < CIRCLE_TOTAL; i++){
-        let circle = document.createElement("li");
-        let circlesContainer = document.querySelector(".circles");
+    const createMuteBtnText = () => {
+        const BTN = document.querySelector(".muteBtn");
+        mute ? BTN.textContent = MUTE_TEXT_MAP[true] : BTN.textContent = MUTE_TEXT_MAP[false];
+    };
 
-        let leftValue = (Math.random() * 100) + "%;";
-        let widthHeight = (Math.random() * 150) + "px;";
-        let animationDelay = (Math.random() * 5) + "s;";
-        let animationDuration = ((Math.random() * 20) + 3) + "s;";
+    const createBubbleBackground = () => {
+        // background bubbles
+        const CIRCLE_TOTAL = 50;
+        const CIRCLE_MIN_MAX_VALUES = {
+            "left": [0, 100],
+            "width": [50, 75],
+            "height": [50, 75],
+            "animationDelay": [0, 5],
+            "animationDuration": [3, 20]
+        }
 
-        circle.classList.add("circle");
-        circle.style.cssText = 
-            "left: " + leftValue + 
-            "width:" + widthHeight + 
-            "height:" + widthHeight + 
-            "animation-delay:" + animationDelay + 
-            "animation-duration:" + animationDuration;
-        
-        circlesContainer.appendChild(circle);
+        const circleCreate = () => {
+            let circle = document.createElement("li");
+            let CIRCLES_CONTAINER = document.querySelector(".circles");
 
-        circle.addEventListener("mousedown", () => {
-            
-            let audio = document.createElement("audio");
-            audio.src = "../data/sound/popEffect.wav";
-            document.body.appendChild(audio);
-            circlesContainer.removeChild(circle);
-            audio.play();
-            audio.onended = () => {
-                document.body.removeChild(audio);
-            }
-        });
+            let leftValue = ((Math.random() * CIRCLE_MIN_MAX_VALUES["left"][1]) + CIRCLE_MIN_MAX_VALUES["left"][0]) + "%;";
+            let width = ((Math.random() * CIRCLE_MIN_MAX_VALUES["width"][1]) + CIRCLE_MIN_MAX_VALUES["width"][0]) + "px;";
+            let height = ((Math.random() * CIRCLE_MIN_MAX_VALUES["height"][1]) + CIRCLE_MIN_MAX_VALUES["height"][0]) + "px;";
+            let animationDelay = ((Math.random() * CIRCLE_MIN_MAX_VALUES["animationDelay"][1]) + CIRCLE_MIN_MAX_VALUES["animationDelay"][0]) + "s;";
+            let animationDuration = ((Math.random() * CIRCLE_MIN_MAX_VALUES["animationDuration"][1]) + CIRCLE_MIN_MAX_VALUES["animationDuration"][0]) + "s;";
+
+            circle.classList.add("circle");
+            circle.style.cssText =
+                "left: " + leftValue +
+                "width:" + width +
+                "height:" + height +
+                "animation-delay:" + animationDelay +
+                "animation-duration:" + animationDuration;
+
+            CIRCLES_CONTAINER.appendChild(circle);
+            circle.addEventListener("mouseover", function popCircle() {
+                console.log("pop")
+                let audio = document.createElement("audio");
+                audio.classList.add("popAudio");
+                audio.src = "../data/sound/popEffect.wav";
+                document.body.appendChild(audio);
+                CIRCLES_CONTAINER.removeChild(circle);
+                audio.muted = true;
+                audio.play();
+                audio.onended = () => {
+                    document.body.removeChild(audio);
+                    circleCreate(); // Replace the circle that was just popped
+                }
+            });
+        }
+
+        for (let i = 0; i < CIRCLE_TOTAL; i++) {
+            circleCreate();
+        }
     }
+
+    createSlider();
+    createColorBtnText();
+    createMuteBtnText();
+    createBubbleBackground();
 
 }
 
